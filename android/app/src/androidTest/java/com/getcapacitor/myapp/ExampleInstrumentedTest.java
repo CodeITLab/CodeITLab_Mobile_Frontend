c2a8ff215761830058d370a6e92194e8ac907da6
Kristijan Fištrek <Kristijan.Fistrek@t.ht.hr>
2020-06-29 21:38:17 +0200

    #1 - Add: Version 1

--- android/app/src/androidTest/java/com/getcapacitor/myapp/ExampleInstrumentedTest.java
+++ android/app/src/androidTest/java/com/getcapacitor/myapp/ExampleInstrumentedTest.java
@@ -1,9 +1,8 @@
 package com.getcapacitor.myapp;
 
 import android.content.Context;
-
 import androidx.test.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
